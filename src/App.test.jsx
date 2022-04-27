956f26d1791242aeeaa96afe8d82edc13fdf438b
Mary Martinez <mary.diana.martinez@gmail.com>
2022-04-27 14:51:41 -0700

    tests are passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,14 +4,16 @@
 import { screen, render } from '@testing-library/react'
 // 🚨
 // import rest
+import {rest} from 'msw'
 // import setupServer
+import {setupServer} from 'msw/node'
 import App from './App'
 
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -20,17 +22,22 @@ const user = {
 }
 
 // 🚨 Create your server
-
+const server = setupServer(
+  rest.get(`https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users`, (req, res, ctx) => res(ctx.json([user])))
+)
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.close())
 
 test('Should render the header', async () => {
   render(<App />)
   const banner = screen.getByRole('banner')
   const headerImg = screen.getByAltText(/alchemy/i)
+  screen.debug();
   const profileName = await screen.findByText(user.name)
 
   expect(banner).toHaveStyle({
@@ -53,7 +60,8 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
   }
 
   // 🚨 Use the server to change the response for this test
-
+  server.use(rest.get(`https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users`, (req, res, ctx) => res(ctx.json([sasuke])))
+  )
   render(<App />)
 
   const profileName = await screen.findByText(sasuke.name)
